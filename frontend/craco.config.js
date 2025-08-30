c25f8b485e4fb39c5c90eb14aba890522a8ed725
renanya <renab@renapc.>
2025-08-30 23:49:42 +1000

    craco

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,7 +1,7 @@
 // craco.config.js
 module.exports = {
   devServer: {
-    host: "0.0.0.0",
+    host: "ec2-3-27-208-10.ap-southeast-2.compute.amazonaws.com",
     port: 8080,
     // Use 'all' to accept any, or list your exact hosts:
     // allowedHosts: ['ec2-XX-XX-XX-XX.ap-southeast-2.compute.amazonaws.com', 'XX.XX.XX.XX']
