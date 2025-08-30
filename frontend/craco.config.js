e90caa51bdfd7a566fb5a7fe42b4b1c1623f8476
renanya <renab@renapc.>
2025-08-31 00:11:11 +1000

    headers

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,10 +1,10 @@
 // craco.config.js
 module.exports = {
   devServer: {
-    host: "ec2-3-27-208-10.ap-southeast-2.compute.amazonaws.com",
+    host: "0.0.0.0",
     port: 8080,
     // Use 'all' to accept any, or list your exact hosts:
     // allowedHosts: ['ec2-XX-XX-XX-XX.ap-southeast-2.compute.amazonaws.com', 'XX.XX.XX.XX']
-    allowedHosts: 'all',
+    allowedHosts: 'ec2-16-176-16-115.ap-southeast-2.compute.amazonaws.com',
   },
 };
--- frontend/src/Baseurl.js
+++ frontend/src/Baseurl.js
@@ -1,4 +1,4 @@
-const baseURL = 'ec2-3-27-208-10.ap-southeast-2.compute.amazonaws.com:8080';
+const baseURL = 'ec2-16-176-16-115.ap-southeast-2.compute.amazonaws.com:3000';
 
 module.exports = {
     baseURL
